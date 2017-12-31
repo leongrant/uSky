70da1dbbeaa21092f6f5d029b19e927b9c039173
Leon Ilia Grant <leon.g_@outlook.com>
2017-12-31 16:20:53 +0100

    Multiplayer Network

---
+++ uSky/Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform playerTransform;
+    public int depth;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform != null)
+        {
+            transform.position = playerTransform.position + new Vector3(0, 0, depth);
+        }
+    }
+
+    public void setTarget(Transform target)
+    {
+        playerTransform = target;
+    }
+}
--- uSky/Assets/Scripts/Player/PlayerMovement.cs
+++ uSky/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,36 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.Networking;
 
-
-[RequireComponent(typeof(CharacterController))]
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
     public float speed = 3.0F;
     public float rotateSpeed = 3.0F;
+
     void Update()
     {
-        CharacterController controller = GetComponent<CharacterController>();
 
+        if(isLocalPlayer) 
+        {
+            MovePlayer();
+        }
+        
+    }
+
+    void MovePlayer()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float curSpeed = speed * Input.GetAxis("Vertical");
         controller.SimpleMove(forward * curSpeed);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        if(isLocalPlayer)
+        {
+            Camera.main.GetComponent<CameraFollowPlayer>().setTarget(gameObject.transform);
+        }
+        
     }
 }
