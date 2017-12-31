7e527389b415dc7d2780fd0f71cd5ffed0328a32
Leon Ilia Grant <leon.g_@outlook.com>
2017-12-31 15:37:15 +0100

    Add CC Movement

---
+++ uSky/Assets/Scripts/Player/LookAtMouse.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class LookAtMouse : MonoBehaviour
+{
+    public float speed;
+
+    void FixedUpdate()
+    {
+
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist = 0.0f;
+
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+            // Smoothly rotate towards the target point.
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+        }
+    }
+}
